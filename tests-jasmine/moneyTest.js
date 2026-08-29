556d3bb84f9cf3cd7467ab53e75a67c68aa19a8d
md tufail anwar <dildarmr887@gmail.com>
2026-08-29 19:35:09 +0530

    Remove default test and money test

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,11 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+describe('test suite: formatCurrency', () =>{
+  it('converts cents into dollar',()=>{
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+
+  it('works with zero',()=>{
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+})
